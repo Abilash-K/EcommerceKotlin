774293b5f6066114481b8b883739c0761b9674f4
Abilah K <abilash.konganar@gmail.com>
2024-08-05 17:23:16 +0530

    Added Login Layout

--- app/src/main/java/com/example/ecommercekotlin/MainActivity.kt
+++ app/src/main/java/com/example/ecommercekotlin/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.ecommercekotlin
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -12,6 +14,11 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+
+        val btn = findViewById<Button>(R.id.button4)
+        btn.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
 
     }
 }
