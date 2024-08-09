329572ff77cda0eb424a45f28a0dbe234b299d41
Abilah K <abilash.konganar@gmail.com>
2024-08-09 10:32:38 +0530

    Feat : Added Validation for the login Screen . Observation : Entirely Completed the Login Part.

--- app/src/main/java/com/example/ecommercekotlin/activity/LoginActivity.kt
+++ app/src/main/java/com/example/ecommercekotlin/activity/LoginActivity.kt
@@ -3,11 +3,8 @@ package com.example.ecommercekotlin.activity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import com.example.ecommercekotlin.R
 import com.example.ecommercekotlin.databinding.ActivityLoginBinding
 import com.example.ecommercekotlin.viewmodel.LoginViewModel
 
@@ -25,28 +22,51 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.loginButton.setOnClickListener {
-            val username = binding.loginEmail.text.toString()
+            val username = binding.loginUsername.text.toString()
             val password = binding.loginPassword.text.toString()
-            loginViewModel.login(username,password)
+           //validation
+            if (username.isEmpty()){
+                //seterror
+                binding.loginUsername.error = "Please enter username"
+                binding.loginUsername.requestFocus()
+                return@setOnClickListener
+            }else if (password.isEmpty()){
+                //seterror
+                binding.loginPassword.error = "Please enter password"
+                binding.loginPassword.requestFocus()
+                return@setOnClickListener
+            }else{
+                binding.loginUsername.error = null
+                binding.loginPassword.error = null
+                loginViewModel.login(username,password)
+            }
+
         }
 
         //loginViewModel
-        loginViewModel.loginResponse.observe(this, Observer { response ->
+        loginViewModel.loginResponse.observe(this) { response ->
             binding.loginProgress.visibility = android.view.View.GONE
-            Toast.makeText(this,"Login Successful: ${response.firstName} ${response.lastName}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                this,
+                "Login Successful: ${response.firstName} ${response.lastName}",
+                Toast.LENGTH_SHORT
+            ).show()
             //Log.d("LoginResponse", "Success: ${response.token}")
-        })
+        }
 
         //LoginError
-        loginViewModel.loginError.observe(this, Observer { error ->
+        loginViewModel.loginError.observe(this) { error ->
             binding.loginProgress.visibility = android.view.View.GONE
-            Toast.makeText(this,"Login Failed: $error", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Login Failed: $error", Toast.LENGTH_SHORT).show()
             Log.d("LoginError", "Error: $error")
-        })
+        }
 
-        loginViewModel.isLoading.observe(this, Observer { isLoading ->
-            binding.loginProgress.visibility = if (isLoading) android.view.View.VISIBLE else android.view.View.GONE
-        })
+        loginViewModel.isLoading.observe(this) { isLoading ->
+            binding.loginProgress.visibility =
+                if (isLoading) android.view.View.VISIBLE else android.view.View.GONE
+        }
 
     }
+
+
 }
